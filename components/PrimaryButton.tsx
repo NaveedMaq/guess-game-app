86b0bbcef21e31523858f486e90c1682eabc5bd5
Naveed <naveed@codeus.in>
2024-08-31 17:03:37 +0530

    feat: Create custom primry button

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,16 +1,48 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 type TPrimaryButtonProps = {
   children: string;
 };
 
 const PrimaryButton: React.FC<TPrimaryButtonProps> = ({ children }) => {
+  function pressHandler() {
+    console.log('Pressed!' + Math.random());
+  }
   return (
-    <View>
-      <Text> {children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) => (pressed ? [styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer)}
+        onPress={pressHandler}
+        android_ripple={{ color: '#640535' }}
+      >
+        <Text style={styles.buttonText}> {children}</Text>
+      </Pressable>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  buttonInnerContainer: {
+    backgroundColor: '#72063c',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+  },
+
+  pressed: {
+    opacity: 0.75,
+  },
+});
 
 export default PrimaryButton;
--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: '#72063c',
+    backgroundColor: '#4e0329',
     borderRadius: 8,
     elevation: 4,
 
