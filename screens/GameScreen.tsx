9f063b86e9c6b65e9a39c8764a1e721c3521a985
Naveed <naveed@codeus.in>
2024-09-07 14:14:37 +0530

    chores: remove ununsed imports

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Alert, StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 import NumberContainer from '@/components/game/NumberContainer';
